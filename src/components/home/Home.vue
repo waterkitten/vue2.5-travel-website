8fb7107c5d1368a2bebe7e731f765ef98d193667
waterbrother2019122132 <56429043+waterbrother2019122132@users.noreply.github.com>
2020-05-14 23:47:19 +0800

    项目的基本配置和搜索框“

---
+++ src/components/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <HomeHeader />
+    </div>
+</template>
+<script>
+import HomeHeader from './pages/Header'
+export default {
+    components: {
+        HomeHeader
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+// js-300ms
+import FastClick from 'fastclick'
+FastClick.attach(document.body);
+// css 
+import 'css/reset.css'
+import 'css/iconfont.css'
+// css简洁路径不用在前面加~     js才要
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
