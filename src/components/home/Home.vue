96e887f49a3d94c59f2010a502e88195a3273ab1
waterbrother2019122132 <56429043+waterbrother2019122132@users.noreply.github.com>
2020-05-17 00:20:18 +0800

    阿贾克斯

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -5,23 +5,34 @@
     <home-icons></home-icons>
     <home-recommend></home-recommend>
     <home-weekend></home-weekend>
-
   </div>
 </template>
 <script>
 import HomeHeader from "./pages/Header";
 import HomeSwiper from "./pages/Swiper";
-import HomeIcons from "./pages/Icons"
-import HomeRecommend from './pages/Recommend'
-import HomeWeekend from './pages/Homeweekend'
+import HomeIcons from "./pages/Icons";
+import HomeRecommend from "./pages/Recommend";
+import HomeWeekend from "./pages/Homeweekend";
+import axios from "axios";
 export default {
   components: {
+    name: "Home",
     HomeHeader,
     HomeSwiper,
     HomeIcons,
     HomeRecommend,
     HomeWeekend
-
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get("/api/index.json").then(this.getHomeInfoSucc);
+    },
+    getHomeInfoSucc(res) {
+      console.log(res);
+    }
+  },
+  mounted() {
+    this.getHomeInfo();
   }
 };
 </script>
